b1343543cc9bada5698930c6caa4e988ae96caa9
Gromuf <luigi.beaumois@gmail.com>
2025-09-16 09:53:17 +0200

    task 0 betty

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -11,7 +11,7 @@
 
 static int is_stable(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
@@ -52,30 +52,30 @@ static void print_grid(int grid[3][3])
 static void topple(int grid[3][3])
 {
 	int tmp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                grid[i][j] -= 4;
-                if (i > 0)
-                    tmp[i - 1][j] += 1;
-                if (i < 2)
-                    tmp[i + 1][j] += 1;
-                if (j > 0)
-                    tmp[i][j - 1] += 1;
-                if (j < 2)
-                    tmp[i][j + 1] += 1;
-            }
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				grid[i][j] -= 4;
+				if (i > 0)
+					tmp[i - 1][j] += 1;
+				if (i < 2)
+					tmp[i + 1][j] += 1;
+				if (j > 0)
+					tmp[i][j - 1] += 1;
+				if (j < 2)
+					tmp[i][j + 1] += 1;
+			}
+		}
+	}
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            grid[i][j] += tmp[i][j];
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid[i][j] += tmp[i][j];
 }
 
 /**
@@ -85,21 +85,19 @@ static void topple(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    /* Step 1: Add both grids cell by cell */
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 
-    /* Step 2: Stabilize the grid */
-    while (!is_stable(grid1))
-    {
-        print_grid(grid1);
-        topple(grid1);
-    }
+	while (!is_stable(grid1))
+	{
+		print_grid(grid1);
+		topple(grid1);
+	}
 }
