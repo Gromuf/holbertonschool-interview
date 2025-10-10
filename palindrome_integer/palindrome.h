16e2554305ab566e5be17c76a841f74fe497f442
Gromuf <luigi.beaumois@gmail.com>
2025-10-10 09:34:30 +0200

    creation du fichier .h

---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,20 @@
+#ifndef _PALINDROME_H
+#define _PALINDROME_H
+
+#include <stddef.h>
+
+/**
+ * is_palindrome - Checks if a given unsigned long integer is a palindrome.
+ * @n: The number to check.
+ *
+ * Description:
+ * A palindrome number reads the same backward and forward.
+ * This function checks whether the given number is a palindrome
+ * without using dynamic memory allocation.
+ *
+ * Return: 1 if n is a palindrome, 0 otherwise.
+ */
+int is_palindrome(unsigned long n);
+
+
+#endif
