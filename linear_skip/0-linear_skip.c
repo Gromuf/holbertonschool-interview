3bbdc59de085baecd6c0188ebecdc1bc9fa7cf9a
Gromuf <luigi.beaumois@gmail.com>
2025-11-21 14:02:43 +0100

    task 0 betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,13 +1,58 @@
 #include "search.h"
 
-static void print_found_range(skiplist_t *start, skiplist_t *end)
+/**
+ * print_range - Prints the search range
+ * @low: Starting node
+ * @high: Ending node
+ */
+static void print_range(skiplist_t *low, skiplist_t *high)
 {
 	printf("Value found between indexes [%lu] and [%lu]\n",
-	       start->index, end->index);
+	       low->index, high->index);
 }
 
 /**
- * linear_skip - Searches for a value in a sorted skip list with express lane
+ * find_express_range - Moves along express lane to find search interval
+ * @head: Pointer to head of list
+ * @value: Value to search for
+ *
+ * Return: pointer to the lower bound of the range
+ */
+static skiplist_t *find_express_range(skiplist_t *head, skiplist_t **high,
+				      int value)
+{
+	skiplist_t *low = head;
+
+	*high = head->express;
+
+	while (*high && (*high)->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       (*high)->index, (*high)->n);
+		low = *high;
+		*high = (*high)->express;
+	}
+
+	if (*high)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       (*high)->index, (*high)->n);
+		print_range(low, *high);
+	}
+	else
+	{
+		skiplist_t *tmp = low;
+
+		while (tmp->next)
+			tmp = tmp->next;
+		print_range(low, tmp);
+	}
+
+	return (low);
+}
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list
  * @head: Pointer to head of skip list
  * @value: Value to search for
  *
@@ -17,34 +62,10 @@ skiplist_t *linear_skip(skiplist_t *head, int value)
 {
 	skiplist_t *low, *high;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	low = head;
-	high = head->express;
-
-	while (high && high->n < value)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-		       high->index, high->n);
-		low = high;
-		high = high->express;
-	}
-
-	if (high)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-		       high->index, high->n);
-		print_found_range(low, high);
-	}
-	else
-	{
-		skiplist_t *temp = low;
-
-		while (temp->next)
-			temp = temp->next;
-		print_found_range(low, temp);
-	}
+	low = find_express_range(head, &high, value);
 
 	while (low && (!high || low->index <= high->index))
 	{
