03f6f440d88672d89c08881a6ce6484d8a3b4e32
Gromuf <luigi.beaumois@gmail.com>
2026-01-29 10:58:08 +0100

    task 0

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in place.
+
+    Args:
+        matrix (list of list of int): The 2D matrix to rotate.
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
