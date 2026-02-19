77d61373d77a5d36ff18573d2cce2c77c2c2dd64
Gromuf <luigi.beaumois@gmail.com>
2026-02-19 10:30:46 +0100

    task 0

---
+++ making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Making change
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given amount total
+    Args:
+        coins (list): list of the values of the coins in your possession
+        total (int): the total amount you need to make change for
+    Returns:
+        int: fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    num_coins = 0
+
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
+
+    return num_coins if total == 0 else -1
