1aaf55bccee6deb073d98bb5aceea8bbadea2bd8
Gromuf <luigi.beaumois@gmail.com>
2026-02-19 10:41:03 +0100

    pycodestyle

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -4,7 +4,7 @@
 
 
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to meet a given amount total
+    """Determine the fewest number of coins needed to meet a given amount total
     Args:
         coins (list): list of the values of the coins in your possession
         total (int): the total amount you need to make change for
@@ -20,5 +20,5 @@ def makeChange(coins, total):
         for i in range(coin, total + 1):
             if dp[i - coin] != float('inf'):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-    
+
     return dp[total] if dp[total] != float('inf') else -1
