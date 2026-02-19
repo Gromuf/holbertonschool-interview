3974f507502a5760435f8b89461f661cf0a6b026
Gromuf <luigi.beaumois@gmail.com>
2026-02-19 10:39:16 +0100

    task 0

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -14,14 +14,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    num_coins = 0
-
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
     for coin in coins:
-        if total <= 0:
-            break
-        count = total // coin
-        num_coins += count
-        total %= coin
-
-    return num_coins if total == 0 else -1
+        for i in range(coin, total + 1):
+            if dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    
+    return dp[total] if dp[total] != float('inf') else -1
