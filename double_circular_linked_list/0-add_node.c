7f5b73c021d289de74a3a491bfb6b21c5a1c6fac
Gromuf <luigi.beaumois@gmail.com>
2026-03-27 10:40:36 +0100

    task 0

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -66,10 +66,25 @@ List *add_node_end(List **list, char *str)
 List *add_node_begin(List **list, char *str)
 {
     List *new_node;
-
-    new_node = add_node_end(list, str);
+    if (!list || !str)
+        return (NULL);
+    new_node = create_node(str);
     if (!new_node)
         return (NULL);
+    
+    if (*list == NULL)
+    {
+        new_node->next = new_node;
+        new_node->prev = new_node;
+    }
+    else
+    {
+        new_node->next = *list;
+        new_node->prev = (*list)->prev;
+        (*list)->prev->next = new_node;
+        (*list)->prev = new_node;
+    }
+
     *list = new_node;
     return (new_node);
 }
