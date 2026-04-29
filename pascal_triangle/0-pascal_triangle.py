27715acf693065addb1d6e1f78786d002df79a75
Gromuf <luigi.beaumois@gmail.com>
2026-04-29 10:57:37 +0200

    fix: remove extra blank line in pascal_triangle function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            
+
         triangle.append(row)
 
     return triangle
